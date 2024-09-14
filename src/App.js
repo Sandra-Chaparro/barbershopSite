ef773b12c3458b9bd7559798e21b9d1f74cbab20
Sandra-Chaparro <sandra.saldepaz@gmail.com>
2024-09-14 13:35:53 -0500

    creation of basic and starting structure: folders and files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {HelmetProvider} from 'react-helmet-async'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import ContactPage from './pages/ContactPage';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
