15a7d004ef752d843c0b8f01055ff9f885460f3b
Sandra Chaparro <sandra.saldepaz@gmail.com>
2024-09-27 14:15:12 -0500

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import './App.css';
-import {HelmetProvider} from 'react-helmet-async'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
-import ContactPage from './pages/ContactPage';
-
+import "./App.css";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <Router>
-       <div className="bg-gray-200 min-h-screen"> {/* Applying background color here */}
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-      <Footer />
+      <div className="flex flex-col bg-gray-200 min-h-screen">
+        {" "}
+        {/* Applying background color here */}
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
