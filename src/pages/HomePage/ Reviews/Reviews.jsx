434be3c525851201b71d998660e5fba339a63ff7
sandra chaparro <sandrachaparro@Mac.lan>
2024-12-18 00:01:40 -0600

    testing for touch sliding on reviews for mobile

--- src/pages/HomePage/ Reviews/Reviews.jsx
+++ src/pages/HomePage/ Reviews/Reviews.jsx
@@ -5,6 +5,7 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
   const [index, setIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState(0); 
   const { id, name, text } = people[index];
   const checkNumber = (number) => {
     if (number > people.length - 1) {
@@ -30,28 +31,42 @@ const Review = () => {
     });
   };
 
+  const handleTouchStart = (e) => {
+    setTouchStart(e.touches[0].clientX); // Get the initial touch position
+  };
+
+  const handleTouchEnd = (e) => {
+    const touchEnd = e.changedTouches[0].clientX; // Get the final touch position
+    if (touchStart - touchEnd > 50) {
+      nextPerson(); // Swipe left to go to the next review
+    }
+    if (touchStart - touchEnd < -50) {
+      prevPerson(); // Swipe right to go to the previous review
+    }
+  };
+
   const minIndex=0;
   const maxIndex=people.length-1;
 
   
-  const getRandomPerson=()=>{
-    const getRandomIntInclusive=(min, max) =>{
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-    }
+  // const getRandomPerson=()=>{
+  //   const getRandomIntInclusive=(min, max) =>{
+  //     min = Math.ceil(min);
+  //     max = Math.floor(max);
+  //     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  //   }
 
-    let randomIndex=getRandomIntInclusive(minIndex, maxIndex);
-    if (randomIndex===index){
-      randomIndex=index+1
-    }
-    setIndex(checkNumber(randomIndex));
+  //   let randomIndex=getRandomIntInclusive(minIndex, maxIndex);
+  //   if (randomIndex===index){
+  //     randomIndex=index+1
+  //   }
+  //   setIndex(checkNumber(randomIndex));
 
-  }
+  // }
   
 
   return (
-    <article className="review">
+    <article className="review" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
       <div className="img-container">
       
         <span className="quote-icon">
