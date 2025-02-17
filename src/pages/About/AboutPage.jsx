4d35b7c6cf6c8404899b6972a80f5e35a667619c
sandra chaparro <sandrachaparro@Mac.lan>
2025-02-17 03:03:58 -0600

    modified background color in pages and fixed about me page

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -5,13 +5,15 @@ import "./About.css"
 export default function AboutPage () {
   return (
     <Layout>
+          <h3 className="aboutPageTitle">About me</h3>
   <div className="about-container">
+
     <div className="about-text">
-      <p> I’m thrilled to introduce myself to you. Cutting hair isn’t just a job for me; my
+      <p className="aboutPara"> I’m thrilled to introduce myself to you. Cutting hair isn’t just a job for me; my
        true passion, and I can’t imagine doing anything else. <br></br><br></br>
        With over two decades of experience, I have honed my skills to provide you
-        with the highest quality grooming services.
-        <br></br><br></br>Book your seat in the chair and let your confidence stand out. This is more than a cut; it’s your signature look.
+        with the highest quality grooming services.<br></br><br></br>
+Book your seat in the chair and let your confidence stand out. This is more than a cut; it’s your signature look.
 
       Step into a world of expert craftsmanship, tailored to suit your unique personality. 
       </p>
